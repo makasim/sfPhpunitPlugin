dfcaff60b1ea538d366336c17eb8095b68a93bb5
Maksim Kotlyar <maksim@maksim-pc.(none)>
2010-08-19 15:28:10 +0300

    fix doctrine fixtures loading

--- lib/fixture/doctrine/sfPhpunitDoctrineBaseData.php
+++ lib/fixture/doctrine/sfPhpunitDoctrineBaseData.php
@@ -3,11 +3,25 @@
 class sfPhpunitDoctrineBaseData extends Doctrine_Data_Import
 {
   protected $_snapshots = array();
+    
+  /**
+   * Do the importing of the data parsed from the fixtures
+   *
+   * @return void
+   */
+  public function doImport($append = false)
+  {
+    $this->_rows = array();
+    
+    return parent::doImport($append);
+  }
   
   public function cleanObjects()
   {
     $this->_importedObjects = array();
     $this->_rows = array();
+    
+    $connection = Doctrine_Manager::getInstance()->getCurrentConnection()->clear();
   }
   
   public function doSnapshot($name)
@@ -18,6 +32,7 @@ class sfPhpunitDoctrineBaseData extends Doctrine_Data_Import
   
   public function loadSnapshot($name)
   {   
-    $this->object_references = self::$_snapshots[$name];
+    $this->_importedObjects = self::$_snapshots[$name]['importedObjects'];
+    $this->_rows = self::$_snapshots[$name]['rows'];
   }
 }

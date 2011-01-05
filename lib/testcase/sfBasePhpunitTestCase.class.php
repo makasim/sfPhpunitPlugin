b29b03204ee18c2b9a3116a78135b657b894fe74
Maksim Kotlyar <maksim@maksim-pc.(none)>
2011-01-05 18:56:24 +0200

    make fixture method publicly visible

--- lib/testcase/sfBasePhpunitTestCase.class.php
+++ lib/testcase/sfBasePhpunitTestCase.class.php
@@ -8,21 +8,21 @@
  * @subpackage testcase
  * @author     Frank Stelzer <dev@frankstelzer.de>
  */
-abstract class sfBasePhpunitTestCase 
+abstract class sfBasePhpunitTestCase
   extends PHPUnit_Framework_TestCase
 {
 	/**
-	 * 
+	 *
 	 * @var sfPhpunitFixture
 	 */
 	protected $_fixture;
-	
+
 	/**
-	 * 
+	 *
 	 * @var array
 	 */
 	protected $_backupSfConfig = array();
-	
+
 	/**
 	 * Dev hook for custom "setUp" stuff
 	 * Overwrite it in your test class, if you have to execute stuff before a test is called.
@@ -38,12 +38,12 @@ abstract class sfBasePhpunitTestCase
 	protected function _end()
 	{
 	}
-	
+
 	protected function _backupSfConfig()
 	{
 	  $this->_backupSfConfig = sfConfig::getAll();
 	}
-	
+
 	protected function _restoreSfConfig()
 	{
 	  sfConfig::clear();
@@ -67,116 +67,116 @@ abstract class sfBasePhpunitTestCase
 		$this->_end();
 		$this->_restoreSfConfig();
 	}
-	
+
   public function getPackageFixtureDir()
   {
     $sep = DIRECTORY_SEPARATOR;
-    
+
     $reflection = new ReflectionClass($this);
     $path = dirname($reflection->getFileName());
-    
+
     return str_replace(
-      "{$sep}test{$sep}phpunit", 
-      "{$sep}test{$sep}phpunit{$sep}fixtures", 
+      "{$sep}test{$sep}phpunit",
+      "{$sep}test{$sep}phpunit{$sep}fixtures",
       $path);
   }
-	
+
 	public function getOwnFixtureDir()
 	{
 	  $sep = DIRECTORY_SEPARATOR;
-	  
+
 	  $reflection = new ReflectionClass($this);
     $path = str_replace('.php', '', $reflection->getFileName());
-    
+
     return str_replace(
-      "{$sep}test{$sep}phpunit", 
-      "{$sep}test{$sep}phpunit{$sep}fixtures", 
+      "{$sep}test{$sep}phpunit",
+      "{$sep}test{$sep}phpunit{$sep}fixtures",
       $path);
 	}
-	
+
 	public function getCommonFixtureDir()
 	{
 	  $path = array(sfConfig::get('sf_test_dir'), 'phpunit', 'fixtures', 'common');
-	  
+
 	  return implode(DIRECTORY_SEPARATOR,$path);
 	}
-	
+
   public function getSymfonyFixtureDir()
   {
     $path = array(sfConfig::get('sf_data_dir'), 'fixtures');
-    
+
     return implode(DIRECTORY_SEPARATOR, $path);
   }
-  
+
   /**
    * All stubed methods should be called at least one time.
    */
   public function getStubStrict($originalClassName, $stubedMethods = array(), $arguments = array(), $mockClassName = '', $callOriginalConstructor = true, $callOriginalClone = true, $callAutoload = true)
   {
     return $this->getStub(
-      $originalClassName, 
-      $stubedMethods, 
-      $arguments, 
-      $mockClassName, 
-      $callOriginalConstructor, 
-      $callOriginalClone, 
-      $callAutoload, 
-      true); 
+      $originalClassName,
+      $stubedMethods,
+      $arguments,
+      $mockClassName,
+      $callOriginalConstructor,
+      $callOriginalClone,
+      $callAutoload,
+      true);
   }
-  
+
   public function getStub($originalClassName, $stubedMethods = array(), $arguments = array(), $mockClassName = '', $callOriginalConstructor = true, $callOriginalClone = true, $callAutoload = true, $strict = false)
   {
     $stubLatter = get_class($this->stubLater());
-    
+
     $mock = $this->getMock(
-      $originalClassName, 
-      array_keys($stubedMethods), 
+      $originalClassName,
+      array_keys($stubedMethods),
       $arguments,
       $mockClassName,
       $callOriginalConstructor,
       $callOriginalClone,
       $callAutoload);
-      
+
     $expects = $strict ? $this->atLeastOnce() : $this->any();
 
     foreach ($stubedMethods as $method => $stub) {
-      
+
       if ($stub instanceof $stubLatter) continue;
-      
+
       $stubedMethod = $mock->expects($expects)->method($method);
       if ($stub instanceof sfCallable) {
         $stubedMethod->will($this->returnCallback($stub->getCallable()));
-      } else if ($stub instanceof Exception) { 
+      } else if ($stub instanceof Exception) {
         $stubedMethod->will($this->throwException($stub));
       } else {
         $stubedMethod->will($this->returnValue($stub));
       }
     }
-    
+
     return $mock;
   }
-  
+
   /**
-   * 
+   *
    * @return sfPhpunitStubLatter
    */
   public function stubLater()
   {
     return new sfPhpunitStubLatter();
   }
-	
+
   /**
-   * 
-   * 
+   *
+   *
    * @return sfPhpunitFixture|mixed
    */
-  protected function fixture($id = null)
+  public function fixture($id = null)
   {
     if (!$this->_fixture) $this->_initFixture();
 
-    return is_null($id) ? $this->_fixture : $this->_fixture->get($id); 
+    return is_null($id) ? $this->_fixture : $this->_fixture->get($id);
   }
-	
+
 	protected function _initFixture(array $options = array())
 	{
 	  $this->_fixture = sfPhpunitFixture::build($this, $options);
